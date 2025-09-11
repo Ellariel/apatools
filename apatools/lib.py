0a889dc396b85168c11ad4b5615c1080309f08e6
Danila Valko <d.v.valko@gmail.com>
2025-09-11 19:34:20 +0200

    modified:   pyproject.toml

--- apatools/lib.py
+++ apatools/lib.py
@@ -2,10 +2,13 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 from statsmodels.stats.inter_rater import aggregate_raters
+import krippendorff
 
 
 
-def gini(values, drop_zeros=False, tolerance=10**-6):
+def gini(values, 
+         drop_zeros=False, 
+         tolerance=10**-6):
     """Calculate the Gini coefficient"""
     # https://github.com/oliviaguest/gini
     # based on bottom eq: http://www.statsdirect.com/help/content/image/stat0206_wmf.gif
@@ -24,7 +27,9 @@ def gini(values, drop_zeros=False, tolerance=10**-6):
     return np.sum((2 * idx - n  - 1) * x) / (n * np.sum(x)) # Gini coefficient
 
 
-def gini_from_percentiles(values, percentiles, values_are_shares=False):
+def gini_from_percentiles(values, 
+                          percentiles, 
+                          values_are_shares=False):
     """
     Calculate the Gini coefficient from percentile-based cumulative data.
     Parameters:
@@ -67,22 +72,25 @@ def z_transform(values, return_params=False, ignore_errors=True):
     return z
 
 
-def fleiss_kappa(df, return_per_item_agreement=False, method="two-tailed"):
+def fleiss_kappa(df, 
+                 return_per_item_agreement=False, 
+                 method="two-tailed"):
     """
     Compute Fleiss' kappa, z-score, and p-value, per-item agreement
-    Null Hypothesis Kappa = 0	Agreement is due to chance
-    0.01-0.02	Slight agreement
-    0.21-0.40	Fair Agreement
-    0.41-0.60	Moderate Agreement
-    0.61-0.80	Substantial Agreement
-    0.81-1.00	Almost Perfect Agreement
-    Negative (Kappa<0)	Agreement less than that expected by chance
+    Null hypothesis: Agreement is due to chance (kappa = 0)
+    0.01-0.02   Slight agreement
+    0.21-0.40   Fair Agreement
+    0.41-0.60   Moderate Agreement
+    0.61-0.80   Substantial Agreement
+    0.81-1.00   Almost Perfect Agreement
+    Negative (kappa < 0): Agreement less than that expected by chance
     """
     # https://en.wikipedia.org/wiki/Fleiss%27s_kappa
     # https://www.statsmodels.org/dev/generated/statsmodels.stats.inter_rater.aggregate_raters.html
 
+    df = df.astype(str) # be careful about NaNs, they are transformed to a category via .astype(str)
     n_items, n_raters = df.shape
-    m = aggregate_raters(df.astype(str), n_cat=None)[0] # be careful about NaNs, they are transformed to a category via .astype(str)
+    m = aggregate_raters(df, n_cat=None)[0] 
     P_i = (np.sum(m**2, axis=1) - n_raters) / (n_raters * (n_raters - 1)) # Per-item agreement
     P_j = np.sum(m, axis=0) / (n_items * n_raters) # Category proportions
     P_e = np.sum(P_j**2) # Expected agreement
@@ -97,7 +105,7 @@ def fleiss_kappa(df, return_per_item_agreement=False, method="two-tailed"):
     z, p = np.nan, np.nan
     if var_kappa > 0:
         z = kappa / np.sqrt(var_kappa)
-        p = norm.sf(np.abs(z)) # one-sided
+        p = norm.sf(np.abs(z)) # one-tailed, agreement > chance
         p = p * 2 if method == "two-tailed" else p
 
     if return_per_item_agreement:
@@ -107,3 +115,47 @@ def fleiss_kappa(df, return_per_item_agreement=False, method="two-tailed"):
     return kappa, z, p
 
 
+def krippendorffs_alpha(df, measurement="nominal", 
+                        return_per_item_agreement=False, 
+                        return_bootstraped_z_score=True,
+                        n_iter_for_bootstrap=100,
+                        bootstrap_seed=13,
+                        method="two-tailed"):
+    """
+    Compute Krippendorff' alpha, z-score, and p-value, per-item agreement
+    Level of measurement = "nominal", "ordinal", "interval", "ratio"
+    Null hypothesis: Agreement is due to chance
+    """
+    # https://en.wikipedia.org/wiki/Krippendorff's_Alpha
+    # https://github.com/pln-fing-udelar/fast-krippendorff
+    
+    df = df.astype(str) # be careful about NaNs, they are transformed to a category via .astype(str)
+    m = aggregate_raters(df, n_cat=None)[0][:,:-1]
+    alpha = krippendorff.alpha(value_counts=m, 
+                               level_of_measurement=measurement)
+    
+    # z-score and p-value
+    z, p = np.nan, np.nan
+    if return_bootstraped_z_score:
+        np.random.seed(bootstrap_seed)
+        null_dist = []
+        for _ in range(n_iter_for_bootstrap):
+            m = aggregate_raters(df.apply(np.random.permutation, axis=0), 
+                                 n_cat=None)[0][:,:-1]
+            null_alpha = krippendorff.alpha(value_counts=m, 
+                               level_of_measurement=measurement)
+            null_dist.append(null_alpha)
+        mu, sigma = np.mean(null_dist), np.std(null_dist)
+        if sigma > 0:
+            z = (alpha - mu) / sigma
+            p = norm.sf(np.abs(z)) # one-tailed, agreement > chance
+            p = p * 2 if method == "two-tailed" else p
+
+    if return_per_item_agreement:
+        agreements = {}
+        for idx, row in df.astype(str).iterrows():
+            row = row.dropna()
+            agreements[idx] = np.nan if len(row) <= 1 else row.value_counts().max() / len(row)
+        return alpha, z, p, pd.Series(agreements, index=df.index, name="per_item_agreement")
+
+    return alpha, z, p 
